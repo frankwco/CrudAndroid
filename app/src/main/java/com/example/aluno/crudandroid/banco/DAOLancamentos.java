cade21d2011dbc0d98e19c4707766809d27f0345
frank <frankwco@gmail.com>
2018-04-20 21:55:20 -0300

    DBCore e DAO

---
+++ app/src/main/java/com/example/aluno/crudandroid/banco/DAOLancamentos.java
@@ -0,0 +1,81 @@
+package com.example.aluno.crudandroid.banco;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.aluno.crudandroid.Lancamento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by aluno on 20/04/18.
+ */
+
+public class DAOLancamentos {
+
+    SQLiteDatabase database;
+
+    public DAOLancamentos(Context context){
+        database = new BDCore(context).getReadableDatabase();
+    }
+
+    public void inserir(Lancamento lancamento){
+        ContentValues values = new ContentValues();
+        values.put("descricao",lancamento.getDescricao());
+        values.put("tipo_lancamento",lancamento.getTipoLancamento());
+        values.put("valor", lancamento.getValorLancamento());
+        database.insert("lancamentos", null, values);
+    }
+    public void excluir(Integer id){
+        database.delete("lancamentos","id=?",
+                new String[]{String.valueOf(id)});
+
+        //database.delete("lancamentos","id="+id,null);
+    }
+    public void alterar(Lancamento lancamento){
+        ContentValues values = new ContentValues();
+        values.put("descricao",lancamento.getDescricao());
+        values.put("tipo_lancamento",lancamento.getTipoLancamento());
+        values.put("valor", lancamento.getValorLancamento());
+        database.update("lancamentos",values,
+                "id="+lancamento.getId(),null);
+    }
+    public List<Lancamento> consultar(){
+        List<Lancamento> lancamentos = new ArrayList<>();
+        String[] colunas = {"id", "descricao", "tipo_lancamento", "valor"};
+        Cursor cursor = database.query("lancamentos", colunas,
+                null,null,null,null,
+                null);
+        cursor.moveToFirst();
+        for(int x=0; x<cursor.getCount(); x++){
+            Lancamento lancamento = new Lancamento();
+            lancamento.setId(cursor.getInt(0));
+            lancamento.setDescricao(cursor.getString(1));
+            lancamento.setTipoLancamento(cursor.getString(2));
+            lancamento.setValorLancamento(cursor.getDouble(3));
+            lancamentos.add(lancamento);
+            cursor.moveToNext();
+        }
+        return lancamentos;
+    }
+
+    public Lancamento buscarPorId(Integer id){
+        Lancamento lancamento = null;
+        String[] colunas = {"id", "descricao", "tipo_lancamento", "valor"};
+        Cursor cursor = database.query("lancamentos", colunas,
+                "id="+id,null,null,null,
+                null);
+        cursor.moveToFirst();
+        if(cursor.getCount()>0){
+            lancamento.setId(cursor.getInt(0));
+            lancamento.setDescricao(cursor.getString(1));
+            lancamento.setTipoLancamento(cursor.getString(2));
+            lancamento.setValorLancamento(cursor.getDouble(3));
+        }
+        return lancamento;
+    }
+
+}
